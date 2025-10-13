1cbbaca15738a0e22573d5556f4be10288a06b0e
LeeSeokWoon <leeseokwoon@naver.com>
2025-10-13 22:34:33 +0900

    Update FactoryMethod.kt

--- src/create/FactoryMethod.kt
+++ src/create/FactoryMethod.kt
@@ -1,5 +1,11 @@
 package create
 
+/**
+ *
+ * 팩토리 메서드(Factory Method) 패턴은 객체 생성 방식을 하위 클래스에 위임
+ * 그러므로 Dialog.render() 에서 createButton을 호출하여 하위클래스에서 해당 createButton 함수를
+ * 작성할수있도록 함
+ */
 interface Button{
     fun render()
     fun onCLick()
@@ -21,7 +27,7 @@ class HTMLButton:Button{
 }
 abstract class Dialog{
     fun render(){
-        val button = this.createButton()
+        val button = createButton()
         button.render()
     }
     abstract fun createButton():Button
